7752bb9460037f3c5b8791a92a676923bca11b95
Ahmed Ahmed <aa7298@fayoum.edu.eg>
2026-03-07 15:58:16 +0200

    final edit

--- Fayora.Contracts/Auth/Requests/VerifyResetPasswordPhoneRequest.cs
+++ Fayora.Contracts/Auth/Requests/VerifyResetPasswordPhoneRequest.cs
@@ -1,6 +1,5 @@
 ﻿namespace Fayora.Contracts.Auth.Requests;
 
 public record VerifyResetPasswordPhoneRequest(
-string PhoneNumber,
-string DeviceId,
-string Code);
+    string PhoneNumber,
+    string Code);
