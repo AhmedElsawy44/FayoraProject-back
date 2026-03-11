f22b1008a997f400c31c5374ef3d078d7da02aee
Ahmed Ahmed <aa7298@fayoum.edu.eg>
2026-03-11 16:34:46 +0200

    Add Unit Owner

---
+++ Fayora.Domain/Entitties/Accommodation/UnitOwner.cs
@@ -0,0 +1,19 @@
+﻿using Fayora.Domain.Common.Entity;
+
+namespace Fayora.Domain.Entitties.Accommodation;
+
+public class UnitOwner : AuditableEntity<Guid>
+{
+    public Guid UserId { get; init; }
+    public float ResponseRate { get; private set; }
+    public int AvgResponseTimeMinutes { get; private set; }
+    public float OwnerRating { get; private set; }
+    public bool IsSuperHost { get; private set; }
+    public int TotalUnitsListed { get; private set; }
+    public string? CommercialName { get; private set; }
+    public string? TaxRegistrationNumber { get; private set; }
+    public bool IsListingEnabled { get; private set; }
+    public DateTimeOffset? VerifiedAt { get; private set; }
+
+
+}
