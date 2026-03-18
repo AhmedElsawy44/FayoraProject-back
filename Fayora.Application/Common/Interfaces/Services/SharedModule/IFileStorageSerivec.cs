71d0f668f1e89bc33c91ec083bd21e42e8c7015b
Ahmed Ahmed <aa7298@fayoum.edu.eg>
2026-03-18 09:07:39 +0200

    fix git conflicts

--- Fayora.Application/Common/Interfaces/Services/SharedModule/IFileStorageSerivec.cs
+++ Fayora.Application/Common/Interfaces/Services/SharedModule/IFileStorageSerivec.cs
@@ -2,5 +2,6 @@
 
 public interface IFileStorageService
 {
+    Task DeleteFileAsync(string mediaURL, CancellationToken cancellationToken);
     Task<string> SaveFileAsync(Stream fileStream, string fileName, string folderName);
 }
