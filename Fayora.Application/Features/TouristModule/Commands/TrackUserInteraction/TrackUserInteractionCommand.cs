93bace962ce49093b46ae8b2ee77e7c5d9ff7406
kareemkhabery9 <kareemkhabery6@gmail.com>
2026-04-25 20:06:30 +0300

    Tracking User Interactions

---
+++ Fayora.Application/Features/TouristModule/Commands/TrackUserInteraction/TrackUserInteractionCommand.cs
@@ -0,0 +1,15 @@
+﻿using Fayora.Domain.Common.Results;
+using Fayora.Domain.Entities.TouristModule;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fayora.Application.Features.TouristModule.Commands.TrackUserInteraction
+{
+    public record TrackUserInteractionCommand(
+        Guid EntityId,
+        EntityType EntityType,
+        InteractionType InteractionType
+    ) : IRequest<Result<Success>>;
+}
---
+++ Fayora.Domain/Common/Events/TouristModule/UserInteractionEvent.cs
@@ -0,0 +1,16 @@
+﻿using Fayora.Domain.Common.Interfaces.IdentityModule;
+using Fayora.Domain.Entities.TouristModule;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fayora.Domain.Common.Events.TouristModule
+{
+    public record UserInteractionEvent
+    (
+        Guid UserId,
+        Guid EntityId,
+        EntityType EntityType,
+        InteractionType InteractionType
+    ) : IDomainEvent;
+}
