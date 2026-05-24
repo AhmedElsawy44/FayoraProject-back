6821a4921d81204e6dad23705cd067e18b755c38
Ahmed-A-Wahba <ahmed.a.wahba@outlook.com>
2026-05-24 22:42:20 +0300

    Update admin/chatbot seeding and DB schema, new migration
    - Refactored seeding to use AdminConstants and ChatbotConstants in new namespace
    - Added User.CreateAdmin method for admin user creation
    - Made TouristInterest constructor private
    - Ignored IsProfileComplete in User EF config and removed from DB
    - Updated connection string for local SQL Server
    - Replaced old migration with new one reflecting schema changes
    - Minor code cleanup and improved cancellation token usage

---
+++ Fayora.Domain/Common/Constants/AdminConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Fayora.Domain.Common.Constants;
+
+public static class AdminConstants
+{
+    public static readonly Guid AdminId = Guid.Parse("f00ef793-2aaa-4033-983f-9d100897d5de");
+    public const string AdminFirstName = "Fayora";
+    public const string AdminLastName = "Admin";
+    public const string AdminEmail = "Fayoratravel@gmail.com";
+    public static readonly string ProfileImageUrl = "https://res.cloudinary.com/dcrymxjvk/image/upload/v1779645957/Fayora_ekinkz.jpg";
+    public static readonly string AdminPassword = "F@y@r@2026";
+}
