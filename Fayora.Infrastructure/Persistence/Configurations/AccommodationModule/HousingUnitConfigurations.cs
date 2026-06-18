bf5206526d0e34e6d0a5508f94b51c14be8033ff
AhmedElsawy44 <ag2107@fayoum.edu.eg>
2026-06-18 19:24:29 +0300

    feat: add VerificationDocumentUrl property to HousingUnit entity and update database schema

--- Fayora.Infrastructure/Persistence/Configurations/AccommodationModule/HousingUnitConfigurations.cs
+++ Fayora.Infrastructure/Persistence/Configurations/AccommodationModule/HousingUnitConfigurations.cs
@@ -46,6 +46,14 @@ public class HousingUnitConfiguration : IEntityTypeConfiguration<HousingUnit>
                   .IsRequired(false);
               });
 
+              builder.OwnsOne(h => h.VerificationDocumentUrl, url =>
+              {
+                     url.Property(u => u.Value)
+                  .HasColumnName("VerificationDocumentUrl")
+                  .HasMaxLength(2048)
+                  .IsRequired(false);
+              });
+
               builder.Property(h => h.CheckInTime).HasColumnType("time");
               builder.Property(h => h.CheckOutTime).HasColumnType("time");
 
